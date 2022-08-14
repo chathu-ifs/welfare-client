9a941f04d6be8e6e4f8df43abe68faba899b9285
Chathu Abeywickrama <chathu.abey93@gmail.com>
2022-08-14 09:39:50 +0400

    Initial commits

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
-function Login() {}
+// Assets
+import Logo from "../../Assets/login-logo.png";
+
+// styles
+import "./Login.scss";
+
+function Login() {
+  return <div>TEST</div>;
+}
 
 export default Login;
